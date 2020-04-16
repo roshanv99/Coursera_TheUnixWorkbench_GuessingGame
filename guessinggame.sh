37ecd06c8bb8e6941cc8dbcde5143888669df6b2
Roshan Varghese <roshan.varghese612@gmail.com>
2020-04-16 22:04:23 +0530

    Input sanitation

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,27 +1,30 @@
 ans=$(ls -a | wc -l)
- 
+
 
 function ask {
-	read v
-	echo $v
+        read v
+        echo $v
 }
 
 while true;
-	do
-		echo "How many files in the directory?"
-		var=$(ask)
-		echo "You entered $var"
-		if [[ $var -eq $ans ]]
-		then
-			echo "Contragulations!"
-			break
-		elif [[ $var -gt $ans ]]
-		then
-			echo "Too HIGH"
-		elif [[ $var -lt $ans ]]
-		then
-			echo "Too LOW"
-		else
-			echo "INVALID"
-		fi
-done
+        do
+                echo "How many files in the directory?"
+                var=$(ask)
+                if [[ $var ]] && [ $var -eq $var 2>/dev/null ]
+                then
+                        echo "You entered $var"
+                        if [[ $var -eq $ans ]]
+                        then
+                                echo "Contragulations!"
+                                break
+                        elif [[ $var -gt $ans ]]
+                        then
+                                echo "Too HIGH"
+                        elif [[ $var -lt $ans ]]
+                        then
+                                echo "Too LOW"
+                        else
+                                echo "INVALID"
+                        fi
+                else
+                        echo 'INVALID INPUT'
